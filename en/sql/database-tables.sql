dcd5ef998e8272edd88df4ffef52e286c292bed2
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-04-23 00:15:24 +0300

    partial update
    -> updated sql to latest versiion made by yorgo
    
    -> made inside specimen.html the desing of the details page
    
    -> made a lil folder structuring in scss  files
    
    -> added 2 new backgrounds

--- en/sql/database-tables.sql
+++ en/sql/database-tables.sql
@@ -1,4 +1,4 @@
-ALTER DATABASE cnam_library CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE dbname CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 DROP TABLE IF EXISTS `book`;
 CREATE TABLE IF NOT EXISTS `book` (
@@ -10,43 +10,81 @@ CREATE TABLE IF NOT EXISTS `book` (
   `PriceSold` decimal(10,2) NOT NULL,
   `Summary` longtext NOT NULL,
   `Quantity` int NOT NULL,
-  `Points` int NOT NULL,
+  `likes` INT NOT NULL DEFAULT 0,
+  `dislikes` INT NOT NULL DEFAULT 0,
   `PublishingHouse` varchar(100) NOT NULL,
   PRIMARY KEY (`IdBook`)
 );
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
-  `IdClient` int NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE IF NOT EXISTS `person` (
   `FirstName` varchar(50) NOT NULL,
   `LastName` varchar(50) NOT NULL,
-  `BirthDate` date NOT NULL,
   `Nationality` varchar(50) NOT NULL,
   `Phone` varchar(20) NOT NULL UNIQUE KEY,
   `Email` varchar(100) NOT NULL UNIQUE KEY,
   `Address` varchar(200) NOT NULL,
-  `Coins` int NOT NULL,
-  PRIMARY KEY (`Idclient`)
+  PRIMARY KEY (`Email`)
 );
+
+DROP TABLE IF EXISTS `supplier`;
+CREATE TABLE IF NOT EXISTS `supplier` (
+  `Email` varchar(20) NOT NULL PRIMARY KEY,
+  `CompanyName` varchar(100) NOT NULL,
+   FOREIGN KEY (`Email`) REFERENCES `person`(`Email`) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `Email` varchar(20) NOT NULL PRIMARY KEY,
+  `CoinsCli` int NOT NULL,
+  `Type` VARCHAR(10) NOT NULL DEFAULT 'client' CHECK (`type` IN ('admin', 'client')),
+  `BirthDate` date NOT NULL,
+  FOREIGN KEY (`Email`) REFERENCES `person`(`Email`) ON DELETE CASCADE
+);
+
 
 DROP TABLE IF EXISTS `buy`;
 CREATE TABLE IF NOT EXISTS `buy` (
-  `IdClient` int NOT NULL,
+  `Email` int NOT NULL,
   `IdBook` int NOT NULL,
   `Date` date NOT NULL,
-  `Quantity` int NOT NULL,
-  FOREIGN KEY (`IdClient`) REFERENCES `client`(`IdClient`) ON DELETE CASCADE,
-  FOREIGN KEY (`IdBook`) REFERENCES `book`(`IdBook`) ON DELETE CASCADE,
-  PRIMARY KEY (`IdClient`,`IdBook`)
+  `Quantity_b` int NOT NULL,
+  FOREIGN KEY (`Email`) REFERENCES `person`(`Email`) ON DELETE CASCADE,
+  FOREIGN KEY (`Email`) REFERENCES `book`(`IdBook`) ON DELETE CASCADE,
+  PRIMARY KEY (`Email`,`IdBook`)
 );
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
-  `IdClient` int NOT NULL,
+  `Email` int NOT NULL,
   `IdBook` int NOT NULL,
   `Comment` text NOT NULL,
   `Likes` int NOT NULL,
-  FOREIGN KEY (`IdClient`) REFERENCES `client`(`IdClient`) ON DELETE CASCADE,
+  FOREIGN KEY (`Email`) REFERENCES `person`(`Email`) ON DELETE CASCADE,
   FOREIGN KEY (`IdBook`) REFERENCES `book`(`IdBook`) ON DELETE CASCADE,
-  PRIMARY KEY (`IdClient`,`IdBook`)
+  PRIMARY KEY (`Email`,`IdBook`)
+);
+
+DROP TABLE IF EXISTS `supply`;
+CREATE TABLE IF NOT EXISTS `supply` (
+  `Email` int NOT NULL,
+  `BookId` varchar(20) NOT NULL,
+  `Date` date NOT NULL,
+  `Quantity_s` int NOT NULL,
+  PRIMARY KEY (`Email`, `BookId`),
+  FOREIGN KEY (`Email`) REFERENCES `person`(`Email`) ON DELETE CASCADE,
+  FOREIGN KEY (`BookId`) REFERENCES `book`(`BookId`) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `borrow`;
+CREATE TABLE IF NOT EXISTS `borrow` (
+  `Email` int NOT NULL,
+  `BookId` varchar(20) NOT NULL,
+  `Borrow_Date` date NOT NULL,
+  `Due_Date` date NOT NULL,
+  `Return_Date` date NOT NULL,
+  PRIMARY KEY (`Email`, `BookId`),
+  FOREIGN KEY (`Email`) REFERENCES `person`(`Email`) ON DELETE CASCADE,
+  FOREIGN KEY (`BookId`) REFERENCES `book`(`BookId`) ON DELETE CASCADE
 );
